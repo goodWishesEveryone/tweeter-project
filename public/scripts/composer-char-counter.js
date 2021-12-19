8d02b770c74df020172c7813d381fb7ce63ed443
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2021-12-19 15:10:57 -0500

    Create a new file composer-char-counter.js, solely responsible for the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+// composer-char-counter.js. is the file solely responsible for character counter.
+
+// function to ensure the DOM has loaded
+$(document).ready(function() {
+  $("#tweet-text").on("input", function() {
+    const count = $(this).val().length;
+    let counter = 140 - count;
+    $(".tweet-button-counter").children(".counter").text(counter);
+    if (counter < 0) {
+      $(".tweet-button-counter").children(".counter").addClass("red");
+    } else {
+      $(".tweet-button-counter").children(".counter").removeClass("red");
+    }
+  });
+});
